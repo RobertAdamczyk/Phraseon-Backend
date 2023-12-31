0a24b12683798b6a8ad66d1c5c8694ffb5fa95f5
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2023-12-31 13:59:35 +0100

    added onUserCreate function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,7 @@
 
 import {onCall, HttpsError} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
@@ -48,6 +49,25 @@ export const createKey = onCall(async (request) => {
       await documentRef.set(newDoc);
       return {message: "Document created successfully."};
     }
+  } catch (error) {
+    throw new HttpsError("unknown", "An error occurred while processing your request.", error);
+  }
+});
+
+export const onUserCreate = functions.auth.user().onCreate(async (user) => {
+  logger.info("onUserCreate", user);
+
+  const newDoc = {
+    "email": user.email,
+    "createdAt": admin.firestore.FieldValue.serverTimestamp(),
+    "name": "",
+    "surname": "",
+  };
+
+  const documentRef = admin.firestore().collection("users").doc(user.uid);
+  try {
+    await documentRef.set(newDoc);
+    return {message: "Document created successfully."};
   } catch (error) {
     throw new HttpsError("unknown", "An error occurred while processing your request.", error);
   }
