baebba952247f3798700882263c8e3367d24f316
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-05 17:59:05 +0100

    add on user delete function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -101,3 +101,33 @@ export const onUserCreate = functions.auth.user().onCreate(async (user) => {
     throw new HttpsError("unknown", "An error occurred while processing your request.", error);
   }
 });
+
+export const onUserDelete = functions.auth.user().onDelete(async (user) => {
+  logger.info("onUserDelete", user);
+
+  // Removing the user's document within Firestore
+  const documentRef = admin.firestore().collection("users").doc(user.uid);
+  await documentRef.delete();
+
+  // Removing the user's ID from 'members' array in all projects within Firestore
+  const projectsRef = admin.firestore().collection("projects");
+  const snapshot = await projectsRef.where("members", "array-contains", user.uid).get();
+
+  if (!snapshot.empty) {
+    const batch = admin.firestore().batch();
+    snapshot.forEach(async (projectDoc) => {
+      // Update the main document
+      const newMembers = projectDoc.data().members.filter((id: string) => id !== user.uid);
+      batch.update(projectDoc.ref, {members: newMembers});
+
+      // Delete the user's document from the nested 'members' collection
+      const memberDocRef = projectDoc.ref.collection("members").doc(user.uid);
+      batch.delete(memberDocRef);
+    });
+    await batch.commit();
+  }
+
+  // Deleting the user's profile picture from Firebase Storage
+  const profilePicPath = "user/" + user.uid + "/image.jpg";
+  await admin.storage().bucket().file(profilePicPath).delete();
+});
