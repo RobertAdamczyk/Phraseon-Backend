fc199eb2f8cc202e29ff2d8e9e30fabaf1f537e3
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-12 22:48:58 +0100

    translate keys on key create

---
+++ functions/src/Domain/Keys/onKeyCreate.ts
@@ -0,0 +1,53 @@
+import {onDocumentCreated} from "firebase-functions/v2/firestore";
+import {HttpsError} from "firebase-functions/v2/https";
+import * as deepl from "deepl-node";
+import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin";
+import {Language} from "../../Model/language";
+
+export const onKeyCreate = onDocumentCreated("projects/{projectId}/keys/{keyId}", async (event) => {
+  logger.info("onCall onKeyCreate", event);
+  const projectId = event.params.projectId;
+  const keyId = event.params.keyId;
+  const db = admin.firestore();
+  const projectRef = db.collection("projects").doc(projectId);
+  const keyRef = projectRef.collection("keys").doc(keyId);
+
+  const snapshot = event.data;
+  if (!snapshot) {
+    console.log("No data associated with the event");
+    return;
+  }
+  const data = snapshot.data();
+
+  const projectDoc = await projectRef.get();
+  const projectData = projectDoc.data();
+
+  if (projectData === undefined || projectData === null) {
+    throw new HttpsError("not-found", "Data not found.");
+  }
+
+  const baseLanguage = projectData.baseLanguage;
+  const textToTranslation = data.translation[baseLanguage];
+  // Base language is already translated
+  const languagesToTranslate = projectData.languages.filter((language: Language) => language !== baseLanguage);
+  const deepLApiKey = process.env.PLANET;
+
+  if (deepLApiKey === undefined || deepLApiKey === null) {
+    return;
+  }
+
+  const translator = new deepl.Translator(deepLApiKey);
+
+  try {
+    for (const language of languagesToTranslate) {
+      const result = await translator.translateText(textToTranslation, null, language as deepl.TargetLanguageCode);
+      const field = "translation." + language;
+      const deeplResult = result as deepl.TextResult;
+      await keyRef.update({[field]: deeplResult.text});
+    }
+  } catch (error) {
+    throw new HttpsError("unknown", "An error occurred while processing your request.", error);
+  }
+  return;
+});
