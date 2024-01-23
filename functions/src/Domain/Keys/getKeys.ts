e68c9cbbf65bc379782adb3407c7b7e0322f99be
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-23 18:19:20 +0100

    getKeys cleanup

--- functions/src/Domain/Keys/getKeys.ts
+++ functions/src/Domain/Keys/getKeys.ts
@@ -1,36 +1,32 @@
 import * as logger from "firebase-functions/logger";
 import {onRequest} from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
+import {isUserProjectMember} from "../../Common/isUserProjectMember";
+import {ErrorCode} from "../../Model/errorCode";
 
 export const getKeys = onRequest(async (request, response) => {
   logger.info("onRequest getKeys", request);
   const db = admin.firestore();
 
-  const projectId = request.query.projectId;
-  const userAccessKey = request.headers["x-access-key"]; // userId
+  const projectId = request.query.projectId as string;
+  const userAccessKey = request.headers["x-access-key"] as string; // userId
 
   if (!projectId) {
-    response.status(400).send("Project ID is required");
+    response.status(400).send(ErrorCode.ProjectNotFound);
     return;
   }
 
   if (!userAccessKey) {
-    response.status(403).send("Access Key is required");
+    response.status(403).send(ErrorCode.PermissionDenied);
     return;
   }
 
   try {
-    const projectRef = db.collection("projects").doc(projectId as string);
-    const projectDoc = await projectRef.get();
-
-    if (!projectDoc.exists) {
-      response.status(403).send("Project not found");
-      return;
-    }
-
-    const projectData = projectDoc.data();
-    if (!projectData?.members.includes(userAccessKey)) {
-      response.status(403).send("User does not have permission for this project");
+    const projectRef = db.collection("projects").doc(projectId);
+  
+    const isProjectMember = await isUserProjectMember(projectId, userAccessKey);
+    if (!isProjectMember) {
+      response.status(403).send(ErrorCode.PermissionDenied);
       return;
     }
 
@@ -54,6 +50,6 @@ export const getKeys = onRequest(async (request, response) => {
     response.status(200).send(transformedKeys);
   } catch (error) {
     console.error("Error:", error);
-    response.status(500).send("Internal Server Error");
+    response.status(500).send(ErrorCode.DatabaseError);
   }
 });
