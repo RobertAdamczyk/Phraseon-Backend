7bc7b6b4931a6ba015e8fe2d5f85937be629c872
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-23 17:05:23 +0100

    set languages technologies refactor

--- functions/src/Domain/Project/setBaseLanguage.ts
+++ functions/src/Domain/Project/setBaseLanguage.ts
@@ -1,38 +1,27 @@
 import {onCall, HttpsError} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
-import {Role} from "../../Model/role";
+import {verifyAuthentication} from "../../Common/verifyAuthentication";
+import {getUserRole} from "../../Common/getUserRole";
+import {Action, assertPermission} from "../../Common/assertPermission";
+import {ErrorCode} from "../../Model/errorCode";
 
 export const setBaseLanguage = onCall(async (request) => {
   logger.info("onCall setBaseLanguage", request.data);
 
-  if (!request.auth) {
-    throw new HttpsError("unauthenticated", "The function must be called while authenticated.");
-  }
-
   const db = admin.firestore();
-  const userId = request.auth.uid;
   const projectId = request.data.projectId;
   const baseLanguage = request.data.baseLanguage;
   const projectRef = db.collection("projects").doc(projectId);
 
-  const currentMemberRef = projectRef.collection("members").doc(userId);
-  const currentMemberDoc = await currentMemberRef.get();
-
-  if (!currentMemberDoc.exists) {
-    throw new HttpsError("not-found", "Current member not found.");
-  }
-
-  const allowedRoles = [Role.admin, Role.owner];
-  const userRole = currentMemberDoc.data()?.role;
-  if (!allowedRoles.includes(userRole)) {
-    throw new HttpsError("failed-precondition", "You can't set the base language of project.");
-  }
+  const userId = verifyAuthentication(request).uid;
+  const role = await getUserRole(projectId, userId);
+  assertPermission(role, Action.setBaseLanguage);
 
   try {
     await projectRef.update({"baseLanguage": baseLanguage});
-    return {message: "Base language changed successfully."};
+    return;
   } catch (error) {
-    throw new HttpsError("unknown", "An error occurred while processing your request.", error);
+    throw new HttpsError("unknown", ErrorCode.DatabaseError);
   }
 });
--- functions/src/Domain/Project/setProjectTechnologies.ts
+++ functions/src/Domain/Project/setProjectTechnologies.ts
@@ -1,44 +1,30 @@
 import {onCall, HttpsError} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
-import {Role} from "../../Model/role";
+import {verifyAuthentication} from "../../Common/verifyAuthentication";
+import {getUserRole} from "../../Common/getUserRole";
+import {Action, assertPermission} from "../../Common/assertPermission";
+import {ErrorCode} from "../../Model/errorCode";
 
 export const setProjectTechnologies = onCall(async (request) => {
   logger.info("onCall setProjectTechnologies", request.data);
 
-  if (!request.auth) {
-    throw new HttpsError("unauthenticated", "The function must be called while authenticated.");
-  }
-
   const db = admin.firestore();
-  const userId = request.auth.uid;
   const projectId = request.data.projectId;
   const technologies = request.data.technologies;
 
+  const userId = verifyAuthentication(request).uid;
+  const role = await getUserRole(projectId, userId);
+  assertPermission(role, Action.setProjectTechnologies);
+
   const projectRef = db.collection("projects").doc(projectId);
-  const projectDoc = await projectRef.get();
-
-  const currentMemberRef = projectRef.collection("members").doc(userId);
-  const currentMemberDoc = await currentMemberRef.get();
-
-  if (!currentMemberDoc.exists) {
-    throw new HttpsError("not-found", "Current member not found.");
-  }
-
-  if (!projectDoc.exists) {
-    throw new HttpsError("not-found", "Project not found.");
-  }
-
-  if (currentMemberDoc.data()?.role !== Role.admin && currentMemberDoc.data()?.role !== Role.owner) {
-    throw new HttpsError("failed-precondition", "You can't change the languages of project.");
-  }
 
   try {
     await projectRef.update({
       "technologies": technologies,
     });
-    return {message: "Changed technologies successfully."};
+    return;
   } catch (error) {
-    throw new HttpsError("unknown", "An error occurred while processing your request.", error);
+    throw new HttpsError("unknown", ErrorCode.DatabaseError);
   }
 });
