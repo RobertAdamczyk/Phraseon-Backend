e8cbee5b7b00d2b8cc4c4386027137e74f6abfc8
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-23 18:02:36 +0100

    isUserProjectOwner refactor

--- functions/src/Domain/User/isUserProjectOwner.ts
+++ functions/src/Domain/User/isUserProjectOwner.ts
@@ -1,25 +1,19 @@
 import {onCall, HttpsError} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
+import {verifyAuthentication} from "../../Common/verifyAuthentication";
+import {ErrorCode} from "../../Model/errorCode";
 
 export const isUserProjectOwner = onCall(async (request) => {
   logger.info("onCall isUserProjectOwner", request.data);
 
-  if (!request.auth) {
-    throw new HttpsError("unauthenticated", "The function must be called while authenticated.");
-  }
-
-  const userId = request.auth.uid;
+  const userId = verifyAuthentication(request).uid;
   const documentsRef = admin.firestore().collection("projects").where("owner", "==", userId);
 
   try {
     const documents = await documentsRef.get();
-    if (!documents.empty) {
-      return {isOwner: true};
-    } else {
-      return {isOwner: false};
-    }
+    return {isOwner: !documents.empty};
   } catch (error) {
-    throw new HttpsError("unknown", "An error occurred while processing your request.", error);
+    throw new HttpsError("unknown", ErrorCode.DatabaseError);
   }
 });
