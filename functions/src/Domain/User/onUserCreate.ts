11425be3fbec9f55ba07bbfd76f99a987e20213b
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-28 02:57:18 +0100

    delete trial on user create

--- functions/src/Domain/User/onUserCreate.ts
+++ functions/src/Domain/User/onUserCreate.ts
@@ -1,7 +1,6 @@
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import {SubscriptionStatus} from "../../Model/subscriptionStatus";
 
 export const onUserCreate = functions.auth.user().onCreate(async (user) => {
   logger.info("onUserCreate", user);
@@ -12,10 +11,8 @@ export const onUserCreate = functions.auth.user().onCreate(async (user) => {
   const newDoc = {
     "email": user.email,
     "createdAt": admin.firestore.FieldValue.serverTimestamp(),
-    "subscriptionValidUntil": subscriptionValidUntil,
     "name": "",
     "surname": "",
-    "subscriptionStatus": SubscriptionStatus.trial,
   };
 
   const documentRef = admin.firestore().collection("users").doc(user.uid);
