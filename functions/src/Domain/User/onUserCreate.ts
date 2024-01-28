80c82441877222ba837757b7fdf4045b6bdefe01
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-28 23:37:06 +0100

    add subscription uuid to user

--- functions/src/Domain/User/onUserCreate.ts
+++ functions/src/Domain/User/onUserCreate.ts
@@ -1,18 +1,19 @@
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+import {v4 as uuidv4} from "uuid";
 
 export const onUserCreate = functions.auth.user().onCreate(async (user) => {
   logger.info("onUserCreate", user);
 
-  const subscriptionValidUntil = new Date();
-  subscriptionValidUntil.setDate(subscriptionValidUntil.getDate() + 7);
+  const subscriptionId = uuidv4();
 
   const newDoc = {
     "email": user.email,
     "createdAt": admin.firestore.FieldValue.serverTimestamp(),
     "name": "",
     "surname": "",
+    "subscriptionId": subscriptionId,
   };
 
   const documentRef = admin.firestore().collection("users").doc(user.uid);
